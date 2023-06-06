374edb7a64d3ee37e0cc32af2489b4a408099c82
hunghx <baby10051999@gmail.com>
2023-06-06 11:28:36 +0700

    sesssion 1 ly thuyet

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    // phương thức main trong java dùng để chạy chương trình
+    public static void main(String[] args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("Hãy nhập vào tuổi của bạn : ");
+//        int age = Integer.parseInt(sc.nextLine());
+//        System.out.println("Hãy nhập vào tên của bạn : ");
+//        String name = sc.nextLine();
+//        System.out.println("Hãy nhập vào sdt của bạn : ");
+//        String phone = sc.nextLine();
+//        System.out.println("Hãy nhập vào số nhà của bạn : ");
+//        int number = Integer.parseInt(sc.nextLine());
+
+        // Hiển thị ra màn hình (output)
+
+//        System.out.print("Hello JV03");
+//        System.out.printf("\t chao ban,thong tin ban vua nhap la name: %s, age: %d, phone: %s, sonha: %d \n",name,age,phone,9);
+//        System.err.println("ERROR ....");
+        System.out.println("\u001B[31mRed textfdfd");
+        System.out.println("\u001B[32mGreen text");
+        System.out.println("\u001B[33mYellow text");
+        System.out.println("\u001B[34mBlue text");
+        System.out.println("\u001B[35mPurple text");
+        System.out.println("\u001B[36mCyan text 😢🤦‍♂️🤦‍♀️🤦‍♀️😜🤞✌️🤷‍♂️😁👍🙌😒😍😊😂🤣❤️⚛️♾️◀️🔼");
+        System.out.println("\u001B[37mWhite text😊😂😂😂🤣🤣❤️");
+
+        // Biến trong java
+        // khai báo biến [datatype] [name variable];
+//
+//        int j,i;
+//        float f;
+//        boolean check;
+//
+//        String str=null;
+//        System.out.println("str co null =>>>"+(str));
+//        // gán giá trị cho biến ;
+//        i=1;
+//        j= 10;
+//        check = true;
+//        str= "Rikkeiacademy";
+//        byte b = 1;
+//        char c ='S';
+//        long l = 100000000;
+//        float fl =1.3f;
+//        double db = 1.4;
+        //  kết hợp khai báo và khởi tạo giá trị cho biến
+//        int k =10;
+        // hằng số : final [datatype] [name constant] = [value]
+//        final int CONSTANT = 100;
+//        System.out.println(CONSTANT);
+        // toán tử ++ / --
+        // nếu ++ đặt trước
+//        int so =10;
+//        int temp = ++so +1;
+//        System.out.println("temp = "+ temp);
+    }
+}
